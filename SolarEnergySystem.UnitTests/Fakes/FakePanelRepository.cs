072be735bd1cbea93e67914598b6d8c5e639191e
Jose Luis David <joseluisdavidgps@gmail.com>
2020-12-16 23:54:20 -0600

    adding test

---
+++ SolarEnergySystem.UnitTests/Fakes/FakePanelRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SolarEnergySystem.Core.Entities;
+using SolarEnergySystem.Core.Enums;
+using SolarEnergySystem.Core.Interfaces;
+
+namespace SolarEnergySystem.UnitTests.Fakes
+{
+    public class FakePanelRepository : IPanelRepository
+    {
+        private readonly IEnumerable<Panel> _panels;
+
+        public FakePanelRepository()
+        {
+            _panels =  new List<Panel>
+            {
+               new Panel {
+                    PanelType= 0,
+                    Longitude= 450.93,
+                    Latitude = -3850.04,
+                    Brand =  "Brand1",
+                    MeasuringUnit = 0,
+                    Id = "A305V5"
+                }
+            };
+
+
+        }
+        public IEnumerable<Panel> GetAllOrderByType()
+        {
+            return _panels.Select(x => x).ToList();
+        }
+    }
+}
---
+++ SolarEnergySystem.UnitTests/PanelServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using SolarEnergySystem.Core.Enums;
+using SolarEnergySystem.Core.Services;
+using SolarEnergySystem.UnitTests.Fakes;
+using Xunit;
+
+namespace SolarEnergySystem.UnitTests
+{
+    public class PanelServiceTest
+    {
+        private PanelServices GetPanelService()
+        {
+            return new PanelServices(new FakePanelRepository());
+        }
+
+        [Fact]
+        public void GetAllOrderByType_Success()
+        {
+            var service = GetPanelService();
+
+            var result = service.GetAllOrderByType();
+
+            Assert.True(result.ResponseCode == ResponseCode.Success);
+            Assert.NotNull(result.Result);
+        }
+    }
+}
