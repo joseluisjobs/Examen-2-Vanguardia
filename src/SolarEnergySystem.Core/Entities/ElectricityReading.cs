63cc5f923e0f2b8b3e644397710b3db643dd3bdf
Jose Luis David <joseluisdavidgps@gmail.com>
2020-12-16 20:15:50 -0600

    Init Project

---
+++ src/SolarEnergySystem.Core/Entities/ElectricityReading.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SolarEnergySystem.Core.DTO;
+
+namespace SolarEnergySystem.Core.Entities
+{
+    public class ElectricityReading : BaseEntity<int>
+    {
+        public string PanelId { get; set; }
+
+        public Panel Panel { get; set; }
+
+        public double KiloWatt { get; set; }
+
+        public DateTime ReadingDateTime { get; set; }
+    }
+}
